640653553af140123f3b7115be92b384d6713cbf
azoath <btech25051.21@bitmesra.ac.in>
2025-12-08 22:00:59 +0530

    complete code for isUnique

--- DSA/src/isUnique.java
+++ DSA/src/isUnique.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class isUnique {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        boolean result = isUnique(str);
+        if(result == true){
+        System.out.println("The " + str + "is unique");
+        }else{
+            System.out.println("The " + str + "is not unique");
+        }
+    }
+
+    public static boolean isUnique(String str) {
+        char[] s = str.toCharArray();
+        Arrays.sort(s);
+        for (int i = 0; i < s.length-1; i++) {
+            if (s[i] == s[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
